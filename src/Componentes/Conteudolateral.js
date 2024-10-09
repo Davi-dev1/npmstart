414d9e93cc55cecbf694f093a886e9e08c8c5dce
Davi_dev1 <davigancalveesdasilva@gmail.com>
2024-10-09 13:59:48 -0300

    Update Conteudolateral.js

--- src/Componentes/Conteudolateral.js
+++ src/Componentes/Conteudolateral.js
@@ -1,10 +1,14 @@
-
 import img  from './imagens/Instagram-Logo-PNG-Image-1.png' ;
 import logo from './imagens/Instagram-Logo.png';
 import Listitem from './Listitem';
 import { FaHouse } from "react-icons/fa6";
 import { IoIosSearch } from "react-icons/io";
 import { SiAzuredataexplorer } from "react-icons/si";
+import { BsFillCameraReelsFill } from "react-icons/bs";
+import { FaHeart } from "react-icons/fa";
+import { RiVideoAddFill } from "react-icons/ri";
+import { CgProfile } from "react-icons/cg";
+
 import { GiTalk } from "react-icons/gi";
 
 import { GrConfigure } from "react-icons/gr";
@@ -21,10 +25,15 @@ export default function Conteudolateral() {
                 <ul className='menulateral'>
                    
                     <Listitem texto='Home' icone={<FaHouse  style={{color:'black'}} /> } />
-                    <Listitem texto='Pesquisar' icone={<IoIosSearch />}/>
-                    <Listitem texto='Explorar' icone={< SiAzuredataexplorer/>}/>
-                    <Listitem texto='Mensagens' icone={<GiTalk />} />
-                    <Listitem texto='Configurações' icone={<GrConfigure />} />
+                    <Listitem texto='Pesquisar' icone={<IoIosSearch style={{color:'black'}} />}/>
+                    <Listitem texto='Explorar' icone={< SiAzuredataexplorer   style={{color:'black'}}  />}/>
+                    <Listitem texto='Reels' icone={<BsFillCameraReelsFill  style={{color:'black'}}   />} />
+                    <Listitem texto='Mensagens' icone={<GiTalk  style={{color:'black'}}   />     } />
+                    <Listitem texto='Notificacões' icone={<FaHeart  style={{color:'black'}}   />} />
+                    <Listitem texto='Criar' icone={<RiVideoAddFill  style={{color:'black'}}   />} />
+                    <Listiitem texto='Perfil' icone={ <CgProfile  style={{color:'black'}} />} />
+
+                    <Listitem texto='Configurações' icone={<GrConfigure  style={{color:'black'}}   />} />
 
                 </ul>
             </div>

e0e86265cd48637a076ae4336f19c425fe5634af
Davi_dev1 <davigancalveesdasilva@gmail.com>
2024-10-10 01:37:20 -0300

    Add files via upload

---
+++ src/Componentes/Post.js
@@ -0,0 +1,39 @@
+import  { CiHeart } from "react-icons/ci";
+import { FaHeart } from "react-icons/fa";
+
+
+
+export default function Post(props) {
+    return (
+
+
+        <div className="Post">
+            <div className="Post-cabeçalho">
+
+
+                
+                <div className="Post-avatar">
+                    <img src="https://picsum.photos/200/300" />
+                    <div className="Post-nome">{props.nome}</div>
+                </div>
+                <div className="Post-conteudo">
+                    <img src={props.foto} />
+                </div>
+                <div className='Post-rodape'>
+                    <div className='Post-curtida'>{ < FaHeart />}</div>
+                    <div className="Post-like">
+                        <div className="Post-comentario">Comentários..</div>
+
+                    </div>
+
+
+                </div>
+
+            </div>
+
+        </div>
+
+
+    );
+
+}
